a8ff96d5495c2ea0682eecd3295d093fa4ae4e17
Sheikh Zain Jaffer <136816448+zainjsheikh900@users.noreply.github.com>
2023-11-14 23:56:23 -0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,228 @@
+// var name1 = "Zain"
+// var name2 = 'Jaffer'
+
+
+// alert(name1+" "+name2)
+
+
+
+
+
+// var num1 = 5
+// var num2 = "10"
+
+// document.write(num1 + num2)
+
+
+
+
+
+// var num1 = 5
+// var num2 = "10"
+
+// document.write(num1 - num2)
+
+
+
+
+
+// var num1 = 5
+// var num2 = "a"
+
+// document.write(num1 - num2)
+
+
+
+
+
+// var num1 = 5
+// var num2 = "a"
+
+// document.write(num1 + num2)
+
+
+
+
+
+// var userInput = +prompt("Enter value 1")
+// var userInput2 = +prompt("Enter value 2")
+
+// var res = (userInput + userInput2)
+
+// document.write(res)
+
+
+
+
+// var num = 5
+// var res = num++ + num
+//            5    +  6
+// document.write(res)
+
+
+
+
+
+// var a = 2
+// var b = 3
+
+// var res = a++ + a + ++b
+
+// document.write(res)
+
+
+
+
+
+
+// var num1 = 6
+// var num2 = 3
+
+// var res = num1++ + num1++ + ++num2 + num2++ + num1++
+//             6    +   7    +   4    +   4    +    8
+// document.write(res)
+
+
+
+
+
+
+
+// var num1 = 6
+// var num2 = 3
+// var num3 = 10
+
+// var res = --num1 - --num2 + num3++ + num3-- + num1
+
+// document.write(res)
+
+
+
+
+// var a = 4
+// var b = 2
+// var c = 10
+
+// var eq = a++ + ++a - --b + c++ + c++ + ++a + a
+
+// document.write(eq)
+
+
+
+
+
+// if(5 == 5){
+//         document.write('han sahi hai')
+
+// }
+
+// else {
+//      document.write("galat hai")
+
+
+// }
+
+
+
+
+
+// var email = prompt('Enter Email');
+// var password = prompt('Enter Password');
+
+// if(email == 'zain' && password == '123456'){
+//         document.write('Login Succesfull')
+
+// }
+// else{
+//         document.write('Invalid Email or Password')
+
+// }
+
+
+
+
+
+
+
+// var email = prompt('Enter Email');
+// var password = prompt('Enter Password');
+
+// if(email == 'zain' || password == '123456'){
+//         document.write('Login Succesfull')
+
+// }
+// else{
+//         document.write('Invalid Email or Password')
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
